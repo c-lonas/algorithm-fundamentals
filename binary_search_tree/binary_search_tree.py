5c8fc33afd91f4af176869935ad56a0976297b0c
Chase Lonas <chaselonas@gmail.com>
2024-06-28 19:16:58 -0400

    initial commit of binary search tree

---
+++ binary_search_tree/binary_search_tree.py
@@ -0,0 +1,9 @@
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.leftChild = None
+        self.rightChild = None
+
+    def PrintTree(self):
+        print(self.data)
