f9f0543c39838622c32852d5030d83d88add771c
Chase Lonas <chaselonas@gmail.com>
2024-06-29 07:52:58 -0400

    add insert method to binary search tree

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -6,4 +6,35 @@ class Node:
         self.rightChild = None
 
     def PrintTree(self):
+        if self.leftChild:
+            self.leftChild.PrintTree()
         print(self.data)
+        if self.rightChild:
+            self.rightChild.PrintTree()
+
+    def insert(self, data):
+        if data < self.data:
+            if self.leftChild:
+                self.leftChild.insert(data)
+            else:
+                self.leftChild = Node(data)
+                return
+            
+        else:
+            if self.rightChild:
+                self.rightChild.insert(data)
+            else:
+                self.rightChild = Node(data)
+                return
+        
+
+
+root = Node(10)
+root.insert(5)
+root.insert(2)
+root.insert(99)
+root.insert(8)
+root.insert(1)
+root.insert(15)
+
+root.PrintTree()
